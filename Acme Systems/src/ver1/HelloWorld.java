2991c617b97b7b90e58b0cefc5cca5b566a2c297
Dave Gibson <davegibson2@gmail.com>
2019-05-23 13:31:23 -0400

    Finished

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,8 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Hello from GitHub");
-
-
+	}
+	
+	public static void heyNow() {
+		System.out.println("hey now!");
 	}
 
 }
