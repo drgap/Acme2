8c8581ee6ebf2aa67106064b94219b7d52664d28
Dave Gibson <davegibson2@gmail.com>
2019-05-24 13:16:51 -0400

    Update HelloWorld.java

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
-		System.out.println("Hello from GitHub2");
+		System.out.println("Hello from asdfdsaf");
 	}
 	
 	public static void heyNow() {
